b4d44f633bf9546d125c42a38e1a06b0923dbbac
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-03-14 22:55:00 -0500

    added functionality to add task button in modal

--- src/create-todo.js
+++ src/create-todo.js
@@ -7,8 +7,25 @@ export class CreateTodo {
   }
 
   showTitle() {
-    console.log(`Is this working? ${this.title} ${this.description}`);
+    console.log(`Is this working? ${this.title}`);
+
+    const addTaskButton = document.querySelector('.add-task-modal-button');
+    const title = document.querySelector('#title');
+    const desc = document.querySelector('#description');
+    const dueDate = document.querySelector('#dueDate');
+    const priority = document.querySelector('#priority');
+    const notes = document.querySelector('#notes');
+
+    addTaskButton.addEventListener('click', () => {
+      console.log(title.value);
+      console.log(desc.value);
+      console.log(dueDate.value);
+      console.log(priority.value);
+      console.log(notes.value);
+    });
   }
+
+  
 
 };
 
