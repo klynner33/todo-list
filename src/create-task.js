215ce63dd85e8e3925aa5dc549f100afbfec14ed
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-03-18 12:27:54 -0500

    Removed displayTask function and put it in its own module

--- src/create-task.js
+++ src/create-task.js
@@ -1,5 +1,3 @@
-
-
 export class CreateTask {
   constructor(title, description, dueDate, priority, notes) {
     this.title = title;
@@ -8,26 +6,4 @@ export class CreateTask {
     this.priority = priority;
     this.notes = notes;
   }
-
-  displayTask() {
-    event.preventDefault();
-    const taskElement = document.createElement('div');
-    taskElement.innerHTML = `
-        <h2>${this.title}</h2>
-        <p>Description: ${this.description}</p>
-        <p>Due Date: ${this.dueDate}</p>
-        <p>Priority: ${this.priority}</p>
-        <p>Notes: ${this.notes}</p>
-    `;
-    const allTasksContainer = document.querySelector('.all-tasks-container');
-    allTasksContainer.appendChild(taskElement);
-   
-    console.log(`${this.title} ${this.description} ${this.dueDate} ${this.priority} ${this.notes}`);
-    const dialog = document.querySelector('.add-todo-modal');
-    dialog.close();
-    const overlay = document.querySelector('.overlay');
-    overlay.style.display = 'none';
-
-  }
-
 };
