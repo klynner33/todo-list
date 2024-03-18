3cb3d615c5f9ebf1d13f3918f735c3a0f97a4adf
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-03-17 22:13:22 -0500

    Added script to clear the form after submitting

--- src/task-modal.js
+++ src/task-modal.js
@@ -1,8 +1,16 @@
+import { CreateTask } from './create-task';
+
 export function addTaskModal() {
 const dialog = document.querySelector('.add-todo-modal');
 const openModalButton = document.querySelector('.add-task-button');
 const closeModalButton = document.querySelector('.close-modal-button');
 const overlay = document.querySelector('.overlay');
+const addButton = document.querySelector('.add-task-modal-button')
+const title = document.querySelector('#title');
+const desc = document.querySelector('#description');
+const dueDate = document.querySelector('#dueDate');
+const priority = document.querySelector('#priority');
+const notes = document.querySelector('#notes');
 
 openModalButton.addEventListener('click', () => {
   dialog.showModal();
@@ -13,4 +21,17 @@ closeModalButton.addEventListener('click', () => {
   dialog.close();
   overlay.style.display = 'none';
 });
+
+addButton.addEventListener("click", () => {
+  event.preventDefault();
+  const newTask = new CreateTask(title.value, desc.value, dueDate.value, priority.value, notes.value);
+    newTask.displayTask();
+    title.value = '';
+    desc.value = '';
+    dueDate.value = '';
+    priority.selectedIndex = 0;
+    notes.value = '';
+});
+
+
 }
