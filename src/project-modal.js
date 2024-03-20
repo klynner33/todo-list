dbb966108975d42706af772a3cac7e49357635e7
klynner33 <113389068+klynner33@users.noreply.github.com>
2024-03-20 08:52:20 -0500

    added project modal module for adding a new project

---
+++ src/project-modal.js
@@ -0,0 +1,35 @@
+import { clearProjectForm } from './clear-form';
+
+export function addProjectModal() {
+  const dialog = document.querySelector('.add-project-modal');
+  const openModalButton = document.querySelector('.fa-plus');
+  const closeModalButton = document.querySelector('.close-project-modal-button');
+  const overlay = document.querySelector('.overlay');
+  const addButton = document.querySelector('.add-project-modal-button')
+  
+  
+  openModalButton.addEventListener('click', () => {
+    dialog.showModal();
+    overlay.style.display = 'block';
+    clearProjectForm();
+  });
+  
+  closeModalButton.addEventListener('click', () => {
+    event.preventDefault();
+    dialog.close();
+    overlay.style.display = 'none';
+    clearProjectForm();
+  });
+  
+  addButton.addEventListener("click", () => {
+    event.preventDefault();
+    // displayTask();
+    const dialog = document.querySelector('.add-project-modal');
+    dialog.close();
+    const overlay = document.querySelector('.overlay');
+    overlay.style.display = 'none';
+    
+  });
+  
+  
+  }
